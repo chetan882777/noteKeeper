dcc139dc832235ddc05bb4d69bb15ffe1d4b2373
chetan <chetan882777@gmail.com>
2018-09-15 12:32:10 +0530

    Instrumented testing ( basic test )

--- app/src/androidTest/java/com/example/android/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/example/android/notekeeper/NoteCreationTest.java
@@ -1,5 +1,6 @@
 package com.example.android.notekeeper;
 
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -7,7 +8,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
 import static org.junit.Assert.*;
+//import static android.support.test.espresso.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static android.support.test.espresso.action.ViewActions.*;
 
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
@@ -17,6 +22,12 @@ public class NoteCreationTest {
 
     @Test
     public void onCreateNote(){
+        //ViewInteraction fabNewNote = onView(withId(R.id.fab));
+        //fabNewNote.perform(click());
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.text_title_note)).perform(typeText("this is the test title"));
+        onView(withId(R.id.text_note_text)).perform(typeText("this is the test note text"),
+                closeSoftKeyboard());
 
     }
 }
