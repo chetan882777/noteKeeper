a11bd9a4ff08c7b00d9f636a17b59f8fa45a9ebd
chetan <chetan882777@gmail.com>
2018-09-15 13:25:02 +0530

    Instrumented testing ( adapter testing with spinner )

--- app/src/androidTest/java/com/example/android/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/example/android/notekeeper/NoteCreationTest.java
@@ -4,30 +4,48 @@ import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static org.junit.Assert.*;
-//import static android.support.test.espresso.*;
+import static android.support.test.espresso.Espresso.*;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
 import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static org.hamcrest.Matchers.*;
+import static android.support.test.espresso.action.ViewActions.pressBack;
 
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
+
+    public static DataManager sDataManager;
+    @BeforeClass
+    public static void classSetUp(){
+        sDataManager = DataManager.getInstance();
+    }
+
     @Rule
     public ActivityTestRule<NoteListActivity> mNoteListActivityRule
             = new ActivityTestRule<>(NoteListActivity.class);
 
     @Test
     public void onCreateNote(){
-        //ViewInteraction fabNewNote = onView(withId(R.id.fab));
-        //fabNewNote.perform(click());
+        final CourseInfo course = sDataManager.getCourse("java_lang");
+        final String noteTitle = "this is the test title";
+        final String noteText = "this is the test note text";
+
+        // be aware of sequence of clicks
+
         onView(withId(R.id.fab)).perform(click());
-        onView(withId(R.id.text_title_note)).perform(typeText("this is the test title"));
-        onView(withId(R.id.text_note_text)).perform(typeText("this is the test note text"),
+
+        onView(withId(R.id.spinner_courses)).perform(click());
+        onData(allOf(instanceOf(CourseInfo.class), equalTo(course))).perform(click());
+
+        onView(withId(R.id.text_title_note)).perform(typeText(noteTitle));
+        onView(withId(R.id.text_note_text)).perform(typeText(noteText),
                 closeSoftKeyboard());
 
+        pressBack();
     }
 }
