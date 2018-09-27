e90e05ba9ab0052acc035f5ca168ea6ab3356d82
chetan <chetan882777@gmail.com>
2018-09-27 23:18:22 +0530

    database ( recyclerAdapter cursor management )

--- app/src/main/java/com/example/android/notekeeper/NoteRecyclerAdapter.java
+++ app/src/main/java/com/example/android/notekeeper/NoteRecyclerAdapter.java
@@ -2,6 +2,7 @@ package com.example.android.notekeeper;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,15 +14,31 @@ import java.util.List;
 public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.Viewholder>{
 
     private final Context mContext;
-    private final List<NoteInfo> mNotes;
+    private Cursor mCursor;
     private final LayoutInflater mLayoutInflater;
 
-    public NoteRecyclerAdapter(Context context, List<NoteInfo> notes) {
+    public NoteRecyclerAdapter(Context context, Cursor cursor) {
         mContext = context;
         mLayoutInflater = LayoutInflater.from(context);
-        mNotes = notes;
+        mCursor = cursor;
+        populateColumnPositions();
     }
 
+    private void populateColumnPositions() {
+        if(mCursor == null){
+            return;
+        }
+        // Get Column Indexes from mCursor
+    }
+
+    public void changeCursor(Cursor cursor){
+        if(mCursor != null){
+            mCursor.close();
+        }
+        mCursor = cursor;
+        populateColumnPositions();
+        notifyDataSetChanged();
+    }
     @Override
     public Viewholder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = mLayoutInflater.inflate(R.layout.note_list_item , parent , false);
