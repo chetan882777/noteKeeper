3d36a087fd9352abc6610fd84ddccd8566c564f4
chetan <chetan882777@gmail.com>
2018-09-29 00:59:12 +0530

    ContentProvider( adding and implementing query to ContentProvider )

---
+++ app/src/main/java/com/example/android/notekeeper/NoteKeeperProvider.java
@@ -0,0 +1,61 @@
+package com.example.android.notekeeper;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+import com.example.android.notekeeper.NoteKeeperDatabaseContract.NoteInfoEntry;
+
+public class NoteKeeperProvider extends ContentProvider {
+
+    private NoteKeeperOpenHelper mOpenHelper;
+
+    public NoteKeeperProvider() {
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // Implement this to handle requests to delete one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        // TODO: Implement this to handle requests for the MIME type of the data
+        // at the given URI.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        // TODO: Implement this to handle requests to insert a new row.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new NoteKeeperOpenHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+
+        Cursor cursor = null;
+        cursor = db.query(NoteInfoEntry.TABLE_NAME , projection , selection , selectionArgs ,
+                null, null , sortOrder);
+
+        return cursor;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        // TODO: Implement this to handle requests to update one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
