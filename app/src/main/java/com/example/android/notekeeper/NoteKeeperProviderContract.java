6e73e5c4d8ef2ac395520247223908715cca8447
chetan <chetan882777@gmail.com>
2018-09-30 00:22:07 +0530

    ContentProvider( adding NoteKeeperProviderContract )

---
+++ app/src/main/java/com/example/android/notekeeper/NoteKeeperProviderContract.java
@@ -0,0 +1,33 @@
+package com.example.android.notekeeper;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public final class NoteKeeperProviderContract {
+    private NoteKeeperProviderContract(){}
+
+    private static final String AUTHORITY = "com.example.android.notekeeper.provider";
+    private static final Uri AUTHORITY_URI = Uri.parse("content://" + AUTHORITY);
+
+    protected interface CourseIdColumn{
+        String COLUMN_COURSE_ID = "course_id";
+    }
+    protected interface NotesColumns{
+        String COLUMN_NOTE_TITLE = "note_title";
+        String COLUMN_NOTE_TEXT = "note_text";
+    }
+
+    protected interface CoursesColumns{
+        String COLUMN_COURSE_TITLE = "course_title";
+    }
+
+    public static final class courses implements BaseColumns , CourseIdColumn , CoursesColumns{
+        public static final String path = "courses";
+        private static final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI , path);
+    }
+
+    public static final class notes implements BaseColumns , CourseIdColumn , NotesColumns{
+        public static final String path = "notes";
+        private static final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI , path);
+    }
+}
