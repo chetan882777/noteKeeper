37a26983c65766a0f3eb48cfd77ccea90f5ce3ec
chetan <chetan882777@gmail.com>
2018-10-01 02:57:23 +0530

    JobScheduler( Launching Job Work in the Background )

--- app/src/main/java/com/example/android/notekeeper/NoteUploaderJobService.java
+++ app/src/main/java/com/example/android/notekeeper/NoteUploaderJobService.java
@@ -4,16 +4,33 @@ import android.app.Service;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.IBinder;
 
 public class NoteUploaderJobService extends JobService {
 
     public static final String EXTRA_DATA_URI = "com.example.android.notekeeper.extra_data_uri";
+    private NoteUploader mNoteUploader;
+
     public NoteUploaderJobService() {
     }
 
     @Override
     public boolean onStartJob(JobParameters params) {
+        AsyncTask<JobParameters , Void , Void> task = new AsyncTask<JobParameters, Void, Void>() {
+            @Override
+            protected Void doInBackground(JobParameters... jobParameters) {
+
+                String stringDatauri = jobParameters[0].getExtras().getString(EXTRA_DATA_URI);
+                Uri datauri = Uri.parse(stringDatauri);
+                mNoteUploader.doUpload(datauri);
+
+                return null;
+            }
+        };
+
+        mNoteUploader = new NoteUploader(this);
         return false;
     }
 
